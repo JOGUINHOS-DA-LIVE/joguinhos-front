2f59031aea65fea2abaa1c81292ad9a308f9f2ec
Pedro Jannuzi <pjannuzi@gmail.com>
2026-07-31 19:49:58 -0300

    feat(home): add navigation to vocabulo on click

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,10 +1,14 @@
 import Titulo from './assets/titulo.png';
 import bonequinho from './assets/bonequinho_de_login.png';
 import twitch from './assets/integracao_twitch.png';
+import { useNavigate } from 'react-router';
 import './Home.css'
 
 
 function Home() {
+
+    const navigate = useNavigate();
+
     return (
         <>
             {/* título */}
@@ -23,7 +27,10 @@ function Home() {
             {/* área dos jogos */}
             <div className="border-5 border-[#BE02E7] shadow-md shadow-yellow-200 h-50 m-30">
                 <div className="grid grid-cols-3 gap-4">
-                    <div className="counter"> Termo </div>
+                    <div 
+                        className="counter"
+                        onClick={() => navigate('/vocabulo')}
+                    > Termo </div>
                     <div className="counter"> Contexto </div>
                     <div className="counter"> Conexo </div>
                 </div>
